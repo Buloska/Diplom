68cfa6438d84c34c9ac4729f7b9b9ff00ff87830
Buloska <bulkascaxarom@inbox.ru>
2025-05-29 19:22:53 +0300

    исправление багов

--- client/src/pages/projects.jsx
+++ client/src/pages/projects.jsx
@@ -57,7 +57,7 @@ const handleLogout = () => {
 };
   const fetchGlobalTasks = useCallback(async () => {
     try {
-      const res = await axios.get('http://localhost:5000/api/tasks', {
+      const res = await axios.get(`${process.env.REACT_APP_API_URL}/api/tasks`, {
         headers: { Authorization: `Bearer ${token}` }
       });
       const global = res.data.filter(task => task.projectId === null);
