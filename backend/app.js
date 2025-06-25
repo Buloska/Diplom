91458c514328d1c2bc592d03f0b3ad5e633e85b1
Buloska <bulkascaxarom@inbox.ru>
2025-06-26 01:21:00 +0300

    bag

--- backend/app.js
+++ backend/app.js
@@ -14,6 +14,10 @@ const subtaskRoutes = require('./routes/subtasks');
 
 
 require('dotenv').config();
+console.log('✅ DB_USER:', process.env.DB_USER);
+console.log('✅ DB_PASS:', process.env.DB_PASS);
+console.log('✅ DB_NAME:', process.env.DB_NAME);
+console.log('✅ DB_HOST:', process.env.DB_HOST);
 
 
 
